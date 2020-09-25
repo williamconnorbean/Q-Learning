b68af22ba23e19994f6a4461a137025e6168cf96
williamconnorbean <beanw@myumanitoba.ca>
2020-09-24 20:31:13 -0500

    Add OpenAi Gym
    - Added the OpenAi Gym `FrozenLake-v0' environment
    - Used the reset() and render() function to ensure it's working

--- qLearning.py
+++ qLearning.py
@@ -1,3 +1,13 @@
 # Q-Learning Implementation
 
-print("Hello World")
+# SFFF       (S: starting point, safe)
+# FHFH       (F: frozen surface, safe)
+# FFFH       (H: hole, fall to your doom)
+# HFFG       (G: goal, where the frisbee is located)
+
+import gym
+import numpy
+
+env = gym.make('FrozenLake-v0')
+env.reset()
+env.render()
